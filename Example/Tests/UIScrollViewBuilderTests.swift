9e9f518d76421e8d790bba336321a6ab36775053
Murat Celebi <mrttcelebi@gmail.com>
2021-03-17 15:05:20 +0300

    add UIScrollViewBuilder  and UIScrollViewBuilderTests

---
+++ Example/Tests/UIScrollViewBuilderTests.swift
@@ -0,0 +1,124 @@
+//
+//  UIScrollViewBuilderTests.swift
+//  MobilliumBuilders_Tests
+//
+//  Created by Murat Celebi on 17.03.2021.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import MobilliumBuilders
+
+class UIScrollViewBuilderTests: XCTestCase {
+
+    func testBackgroundColor() {
+        let backgroundColor = UIColor.lightGray
+        let scrollView = UIScrollViewBuilder()
+            .backgroundColor(backgroundColor)
+            .build()
+        
+        XCTAssertEqual(scrollView.backgroundColor, backgroundColor)
+    }
+    
+    func testShowsVerticalScrollIndicator() {
+        let scrollView = UIScrollViewBuilder()
+            .showsVerticalScrollIndicator(true)
+            .build()
+        
+        XCTAssertTrue(scrollView.showsVerticalScrollIndicator)
+        XCTAssertEqual(scrollView.showsVerticalScrollIndicator, true)
+    }
+    
+    func testShowsHorizontalScrollIndicator() {
+        let scrollView = UIScrollViewBuilder()
+            .showsHorizontalScrollIndicator(false)
+            .build()
+        
+        XCTAssertFalse(scrollView.showsHorizontalScrollIndicator)
+        XCTAssertEqual(scrollView.showsHorizontalScrollIndicator, false)
+    }
+
+    func testContentInset() {
+        let contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        let scrollView = UIScrollViewBuilder()
+            .contentInset(contentInset)
+            .build()
+        
+        XCTAssertTrue(scrollView.contentInset == contentInset)
+        XCTAssertTrue(scrollView.contentInset.left == contentInset.left)
+    }
+    
+    func testIndicatorStyle() {
+        let indicatorStyle = UIScrollView.IndicatorStyle.black
+        let scrollView = UIScrollViewBuilder()
+            .indicatorStyle(indicatorStyle)
+            .build()
+        
+        XCTAssertTrue(scrollView.indicatorStyle == indicatorStyle)
+    }
+    
+    func testKeyboardDismissMode() {
+        let keyboardDismissMode = UIScrollView.KeyboardDismissMode.onDrag
+        let scrollView = UIScrollViewBuilder()
+            .keyboardDismissMode(keyboardDismissMode)
+            .build()
+        
+        XCTAssertTrue(scrollView.keyboardDismissMode == keyboardDismissMode)
+    }
+    
+    @available(iOS 11.0, *)
+    func testContentInsetAdjustmentBehavior() {
+        let contentInsetAdjustmentBehavior = UIScrollView.ContentInsetAdjustmentBehavior.automatic
+        let scrollView = UIScrollViewBuilder()
+            .contentInsetAdjustmentBehavior(contentInsetAdjustmentBehavior)
+            .build()
+        
+        XCTAssertTrue(scrollView.contentInsetAdjustmentBehavior == contentInsetAdjustmentBehavior)
+    }
+    
+    func testBounces() {
+        let scrollView = UIScrollViewBuilder()
+            .bounces(true)
+            .build()
+        
+        XCTAssertTrue(scrollView.bounces)
+        XCTAssertEqual(scrollView.bounces, true)
+    }
+    
+    func testAlwaysBounceVertical() {
+        let scrollView = UIScrollViewBuilder()
+            .alwaysBounceVertical(false)
+            .build()
+        
+        XCTAssertFalse(scrollView.alwaysBounceVertical)
+        XCTAssertEqual(scrollView.alwaysBounceVertical, false)
+    }
+    
+    func testAlwaysBounceHorizontal() {
+        let scrollView = UIScrollViewBuilder()
+            .alwaysBounceHorizontal(true)
+            .build()
+        
+        XCTAssertTrue(scrollView.alwaysBounceHorizontal)
+        XCTAssertEqual(scrollView.alwaysBounceHorizontal, true)
+    }
+    
+    func testIsPagingEnabled() {
+        let scrollView = UIScrollViewBuilder()
+            .isPagingEnabled(false)
+            .build()
+        
+        XCTAssertFalse(scrollView.isPagingEnabled)
+        XCTAssertEqual(scrollView.isPagingEnabled, false)
+    }
+    
+    func testIsScrollEnabled() {
+        let scrollView = UIScrollViewBuilder()
+            .isScrollEnabled(true)
+            .build()
+        
+        XCTAssertTrue(scrollView.isScrollEnabled)
+        XCTAssertEqual(scrollView.isScrollEnabled, true)
+    }
+    
+}
---
+++ MobilliumBuilders/Classes/UIScrollViewBuilder.swift
@@ -0,0 +1,93 @@
+//
+//  UIScrollViewBuilder.swift
+//  MobilliumBuilders
+//
+//  Created by Murat Çelebi on 17.03.2021.
+//
+
+import UIKit
+
+public class UIScrollViewBuilder<T: UIScrollView> {
+    
+    private let scrollView = T()
+    
+    public init() {}
+       
+    @discardableResult
+    public func backgroundColor(_ backgroundColor: UIColor) -> Self {
+        scrollView.backgroundColor = backgroundColor
+        return self
+    }
+    
+    @discardableResult
+    public func showsVerticalScrollIndicator(_ showsVerticalScrollIndicator: Bool) -> Self {
+        scrollView.showsVerticalScrollIndicator = showsVerticalScrollIndicator
+        return self
+    }
+    
+    @discardableResult
+    public func showsHorizontalScrollIndicator(_ showsHorizontalScrollIndicator: Bool) -> Self {
+        scrollView.showsHorizontalScrollIndicator = showsHorizontalScrollIndicator
+        return self
+    }
+    
+    @discardableResult
+    public func contentInset(_ contentInset: UIEdgeInsets) -> Self {
+        scrollView.contentInset = contentInset
+        return self
+    }
+    
+    @discardableResult
+    public func indicatorStyle(_ indicatorStyle: UIScrollView.IndicatorStyle) -> Self {
+        scrollView.indicatorStyle = indicatorStyle
+        return self
+    }
+    
+    @discardableResult
+    public func keyboardDismissMode(_ keyboardDismissMode: UIScrollView.KeyboardDismissMode) -> Self {
+        scrollView.keyboardDismissMode = keyboardDismissMode
+        return self
+    }
+    
+    @available(iOS 11.0, *)
+    @discardableResult
+    public func contentInsetAdjustmentBehavior(_ contentInsetAdjustmentBehavior: UIScrollView.ContentInsetAdjustmentBehavior) -> Self {
+        scrollView.contentInsetAdjustmentBehavior = contentInsetAdjustmentBehavior
+        return self
+    }
+    
+    @discardableResult
+    public func bounces(_ bounces: Bool) -> Self {
+        scrollView.bounces = bounces
+        return self
+    }
+    
+    @discardableResult
+    public func alwaysBounceVertical(_ alwaysBounceVertical: Bool) -> Self {
+        scrollView.alwaysBounceVertical = alwaysBounceVertical
+        return self
+    }
+    
+    @discardableResult
+    public func alwaysBounceHorizontal(_ alwaysBounceHorizontal: Bool) -> Self {
+        scrollView.alwaysBounceHorizontal = alwaysBounceHorizontal
+        return self
+    }
+    
+    @discardableResult
+    public func isPagingEnabled(_ isPagingEnabled: Bool) -> Self {
+        scrollView.isPagingEnabled = isPagingEnabled
+        return self
+    }
+    
+    @discardableResult
+    public func isScrollEnabled(_ isScrollEnabled: Bool) -> Self {
+        scrollView.isScrollEnabled = isScrollEnabled
+        return self
+    }
+    
+    public func build() -> T {
+        return scrollView
+    }
+    
+}
