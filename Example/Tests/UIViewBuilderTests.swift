7b234a07669e57fa725a410f5352a5135a66cc69
Murat Celebi <mrttcelebi@gmail.com>
2021-03-18 10:38:52 +0300

    add UIViewBuilder and UIViewBuilderTests

---
+++ Example/Tests/UIViewBuilderTests.swift
@@ -0,0 +1,118 @@
+//
+//  UIViewBuilderTests.swift
+//  MobilliumBuilders_Tests
+//
+//  Created by Murat Celebi on 18.03.2021.
+//  Copyright © 2021 Mobillium. All rights reserved.
+//
+
+import XCTest
+import MobilliumBuilders
+
+class UIViewBuilderTests: XCTestCase {
+
+    func testBackgroundColor() {
+        let backgroundColor = UIColor.red
+        let view = UIViewBuilder()
+            .backgroundColor(backgroundColor)
+            .build()
+        
+        XCTAssertEqual(view.backgroundColor, backgroundColor)
+    }
+    
+    func testCornerRadius() {
+        let cornerRadius: CGFloat = 10
+        let view = UIViewBuilder()
+            .cornerRadius(cornerRadius)
+            .build()
+        
+        XCTAssertEqual(view.layer.cornerRadius, cornerRadius)
+    }
+    
+    func testClipsToBounds() {
+        let view = UIViewBuilder()
+            .clipsToBounds(true)
+            .build()
+        
+        XCTAssertTrue(view.clipsToBounds)
+    }
+    
+    func testBorderColor() {
+        let borderColor = UIColor.red.cgColor
+        let view = UIViewBuilder()
+            .borderColor(borderColor)
+            .build()
+        
+        XCTAssertEqual(view.layer.borderColor, borderColor)
+    }
+    
+    func testBorderWidth() {
+        let borderWidth: CGFloat = 3
+        let view = UIViewBuilder()
+            .borderWidth(borderWidth)
+            .build()
+        
+        XCTAssertEqual(view.layer.borderWidth, borderWidth)
+    }
+    
+    func testShadowColor() {
+        let shadowColor = UIColor.systemPink.cgColor
+        let view = UIViewBuilder()
+            .shadowColor(shadowColor)
+            .build()
+        
+        XCTAssertEqual(view.layer.shadowColor, shadowColor)
+    }
+    
+    func testShadowOffset() {
+        let shadowOffset = CGSize(width: 3, height: 3)
+        let view = UIViewBuilder()
+            .shadowOffset(shadowOffset)
+            .build()
+        
+        XCTAssertEqual(view.layer.shadowOffset, shadowOffset)
+    }
+    
+    func testShadowRadius() {
+        let shadowRadius: CGFloat = 8
+        let view = UIViewBuilder()
+            .shadowRadius(shadowRadius)
+            .build()
+        
+        XCTAssertEqual(view.layer.shadowRadius, shadowRadius)
+    }
+    
+    func testShadowOpacity() {
+        let shadowOpacity: Float = 0.4
+        let view = UIViewBuilder()
+            .shadowOpacity(shadowOpacity)
+            .build()
+        
+        XCTAssertEqual(view.layer.shadowOpacity, shadowOpacity)
+    }
+    
+    func testAlpha() {
+        let alpha: CGFloat = 1
+        let view = UIViewBuilder()
+            .alpha(alpha)
+            .build()
+        
+        XCTAssertEqual(view.alpha, alpha)
+    }
+    
+    func testIsOpaque() {
+        let view = UIViewBuilder()
+            .isOpaque(false)
+            .build()
+        
+        XCTAssertFalse(view.isOpaque)
+    }
+
+    func testBuildClearBackgroundView() {
+        let backgroundColor = UIColor.clear
+        let view = UIViewBuilder.buildClearBackgroundView()
+        
+        XCTAssertEqual(view.backgroundColor, backgroundColor)
+    }
+    
+}
---
+++ MobilliumBuilders/Classes/UIViewBuilder.swift
@@ -0,0 +1,97 @@
+//
+//  UIViewBuilder.swift
+//  MobilliumBuilders
+//
+//  Created by Murat Celebi on 18.03.2021.
+//  Copyright © 2021 Mobillium. All rights reserved.
+//
+
+import UIKit
+
+public class UIViewBuilder<T: UIView> {
+    
+    private let view = T()
+    
+    public init() {}
+    
+    @discardableResult
+    public func backgroundColor(_ backgroundColor: UIColor) -> Self {
+        view.backgroundColor = backgroundColor
+        return self
+    }
+    
+    @discardableResult
+    public func cornerRadius(_ cornerRadius: CGFloat) -> Self {
+        view.layer.cornerRadius = cornerRadius
+        return self
+    }
+    
+    @discardableResult
+    public func clipsToBounds(_ clipsToBounds: Bool) -> Self {
+        view.clipsToBounds = clipsToBounds
+        return self
+    }
+    
+    @discardableResult
+    public func borderColor(_ borderColor: CGColor) -> Self {
+        view.layer.borderColor = borderColor
+        return self
+    }
+    
+    @discardableResult
+    public func borderWidth(_ borderWidth: CGFloat) -> Self {
+        view.layer.borderWidth = borderWidth
+        return self
+    }
+    
+    @discardableResult
+    public func shadowColor(_ shadowColor: CGColor) -> Self {
+        view.layer.shadowColor = shadowColor
+        return self
+    }
+    
+    @discardableResult
+    public func shadowOffset(_ shadowOffset: CGSize) -> Self {
+        view.layer.shadowOffset = shadowOffset
+        return self
+    }
+    
+    @discardableResult
+    public func shadowRadius(_ shadowRadius: CGFloat) -> Self {
+        view.layer.shadowRadius = shadowRadius
+        return self
+    }
+    
+    @discardableResult
+    public func shadowOpacity(_ shadowOpacity: Float) -> Self {
+        view.layer.shadowOpacity = shadowOpacity
+        return self
+    }
+    
+    @discardableResult
+    public func alpha(_ alpha: CGFloat) -> Self {
+        view.alpha = alpha
+        return self
+    }
+    
+    @discardableResult
+    public func isOpaque(_ isOpaque: Bool) -> Self {
+        view.isOpaque = isOpaque
+        return self
+    }
+    
+    public func build() -> T {
+        return view
+    }
+    
+}
+
+public extension UIViewBuilder {
+    
+    static func buildClearBackgroundView() -> UIView {
+        return UIViewBuilder()
+            .backgroundColor(.clear)
+            .build()
+    }
+    
+}
