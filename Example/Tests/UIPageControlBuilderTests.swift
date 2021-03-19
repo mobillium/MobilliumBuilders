b9e7620307a196fcec1103a488c05bc87d3be35c
Murat Celebi <mrttcelebi@gmail.com>
2021-03-19 15:24:42 +0300

    add UIPageControlBuilder and UIPageControlBuilderTests

---
+++ Example/Tests/UIPageControlBuilderTests.swift
@@ -0,0 +1,102 @@
+//
+//  UIPageControlBuilderTests.swift
+//  MobilliumBuilders_Tests
+//
+//  Created by Murat Celebi on 19.03.2021.
+//  Copyright © 2021 Mobillium. All rights reserved.
+//
+
+import XCTest
+import MobilliumBuilders
+
+class UIPageControlBuilderTests: XCTestCase {
+
+    func testNumberOfPages() {
+        let numberOfPages = 5
+        let pageControl = UIPageControlBuilder()
+            .numberOfPages(numberOfPages)
+            .build()
+        
+        XCTAssertEqual(pageControl.numberOfPages, numberOfPages)
+    }
+    
+    func testHidesForSinglePage() {
+        let pageControl = UIPageControlBuilder()
+            .hidesForSinglePage(true)
+            .build()
+        
+        XCTAssertTrue(pageControl.hidesForSinglePage)
+    }
+    
+    func testPageIndicatorTintColor() {
+        let pageIndicatorTintColor = UIColor.red
+        let pageControl = UIPageControlBuilder()
+            .pageIndicatorTintColor(pageIndicatorTintColor)
+            .build()
+        
+        XCTAssertEqual(pageControl.pageIndicatorTintColor, pageIndicatorTintColor)
+    }
+    
+    func testCurrentPageIndicatorTintColor() {
+        let currentPageIndicatorTintColor = UIColor.yellow
+        let pageControl = UIPageControlBuilder()
+            .currentPageIndicatorTintColor(currentPageIndicatorTintColor)
+            .build()
+        
+        XCTAssertEqual(pageControl.currentPageIndicatorTintColor, currentPageIndicatorTintColor)
+    }
+    
+    @available(iOS 14.0, *)
+    func testBackgroundStyle() {
+        let backgroundStyle = UIPageControl.BackgroundStyle.automatic
+        let pageControl = UIPageControlBuilder()
+            .backgroundStyle(backgroundStyle)
+            .build()
+        
+        XCTAssertEqual(pageControl.backgroundStyle, backgroundStyle)
+    }
+    
+    @available(iOS 14.0, *)
+    func testPreferredIndicatorImage() {
+        let preferredIndicatorImage = UIImage(named: "ic_face")
+        let pageControl = UIPageControlBuilder()
+            .preferredIndicatorImage(preferredIndicatorImage)
+            .build()
+        
+        XCTAssertEqual(pageControl.preferredIndicatorImage, preferredIndicatorImage)
+    }
+    
+    func testIsUserInteractionEnabled() {
+        let pageControl = UIPageControlBuilder()
+            .isUserInteractionEnabled(true)
+            .build()
+        
+        XCTAssertTrue(pageControl.isUserInteractionEnabled)
+    }
+    
+    func testIsHidden() {
+        let pageControl = UIPageControlBuilder()
+            .isHidden(false)
+            .build()
+        
+        XCTAssertFalse(pageControl.isHidden)
+    }
+    
+    func testIsAccessibilityElement() {
+        let pageControl = UIPageControlBuilder()
+            .isAccessibilityElement(true)
+            .build()
+        
+        XCTAssertTrue(pageControl.isAccessibilityElement)
+    }
+    
+    func testAccessibilityIdentifier() {
+        let accessibilityIdentifier = "pageControl"
+        let pageControl = UIPageControlBuilder()
+            .accessibilityIdentifier(accessibilityIdentifier)
+            .build()
+        
+        XCTAssertEqual(pageControl.accessibilityIdentifier, accessibilityIdentifier)
+    }
+    
+}
